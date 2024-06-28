dc51b2e59feaa9097e28102c53ea0c28d8c04090
ery <128942912+erutm@users.noreply.github.com>
2024-06-28 21:44:36 +0700

    Update syarat_pendaftaran1.php

--- dashboard/include/syarat_pendaftaran.php
+++ dashboard/include/syarat_pendaftaran.php
@@ -1,9 +1,12 @@
+<!---nambah heading-->
+<h2>Syarat Pendaftaran</h2>
 <div class="row">
     <div class="col-sm-12 col-md-8 col-lg-10 col-lg-offset-1">
         <div class="card" style="margin-top: 50px">
             <div class="card-header" data-background-color="gb">
                 <h4 class="title">Syarat Pendaftaran</h4>
-                <p class="category">Isi Form pendaftaran dengan benar</p>
+                <!---edit tulisan-->
+                <p class="category">Isi Form Pendaftaran dengan benar</p>
             </div>
             <div class="card-content">
                 <?php
@@ -19,7 +22,7 @@
                     if ($daftar['status_pendaftaran'] == 1) {
                         echo "<div class='alert alert-success alert-dismissable'>
                               <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
-                              <strong>Selamat!</strong> pendaftaran anda sudah dikonfirmasi Admin. Selanjutnya, cetak kwitansi pembayaran <a href='index.php?page=9'><u>di menu pembayaran</u></a>. dan lakukan konfirmasi pembayaran setelah melakukan pembayaran.
+                              <strong>Selamat!</strong> pendaftaran Anda sudah dikonfirmasi Admin. Selanjutnya, cetak kwitansi pembayaran <a href='index.php?page=9'><u>di menu pembayaran</u></a>. dan lakukan konfirmasi pembayaran setelah melakukan pembayaran.
                             </div>";
 
                         // echo '<a href="../assets/uploads/kwitansi-pembayaran.jpeg" class="btn btn-primary btn-md pull-left" download><i class="fa fa-print"></i> Cetak biaya yang harus dibayar untuk pendaftaran</a>';
@@ -35,15 +38,15 @@
                     } else if ($daftar['status_pendaftaran'] == 0) {
                         echo "<div class='alert alert-warning alert-dismissable'>
                               <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
-                              <strong>Persyaratan sudah lengkap. tunggu konfirmasi admin paling lambat 2 hari kerja</strong> 
+                              <strong>Persyaratan sudah lengkap. tunggu konfirmasi Admin paling lambat 2 hari kerja</strong> 
                             </div>";
                     }
                 }
                 ?>
 
 
-
-                <h3>Berikut adalah syarat pendaftaran siswa baru yang harus dipenuhi :</h3>
+                <!--edit teks-->
+                <h3>Berikut adalah syarat pendaftaran yang harus dipenuhi:</h3>
                 <ol>
                     <li>
                         <font color="#2ecc71">Mengisi Formulir Pendaftaran <i class="fa fa-check"></font></i>
@@ -56,12 +59,12 @@
                             if ($daftar['status_pendaftaran'] == 1) {
                                 echo '<font color="#2ecc71">a<i class="fa fa-check"></i></font>';
                             } else if ($daftar['status_pendaftaran'] >= 2) {
-                                echo '<font color="#2ecc71">Upload Surat Pengantar Magang Dari Sekolah / Kampus<i class="fa fa-check"></i></font>';
+                                echo '<font color="#2ecc71">Upload Surat Pengantar Magang dari Sekolah/Perguruan Tinggi<i class="fa fa-check"></i></font>';
                             } else {
-                                echo '<font color="#2ecc71">Upload Surat Pengantar Magang Dari Sekolah / Kampus<i class="fa fa-check"></i></font> <a href="index.php?page=5" class="btn btn-primary btn-sm" title="Upload Akte Kelahiran dan Kartu Keluarga"><i class="fa fa-pencil"></i></a>';
+                                echo '<font color="#2ecc71">Upload Surat Pengantar Magang dari Sekolah/Perguruan Tinggi<i class="fa fa-check"></i></font> <a href="index.php?page=5" class="btn btn-primary btn-sm" title="Upload Akte Kelahiran dan Kartu Keluarga"><i class="fa fa-pencil"></i></a>';
                             }
                         } else {
-                            echo 'Upload Surat Pengantar Magang Dari Sekolah / Kampus <a href="index.php?page=5" class="btn btn-primary btn-sm" title="Upload Akte Kelahiran dan Kartu Keluarga"><i class="fa fa-upload"></i></a>';
+                            echo 'Upload Surat Pengantar Magang dari Sekolah/Perguruan Tinggi <a href="index.php?page=5" class="btn btn-primary btn-sm" title="Upload Akte Kelahiran dan Kartu Keluarga"><i class="fa fa-upload"></i></a>';
                         }
 
                         ?>
@@ -86,7 +89,7 @@
                     </li>
                 </ol>
 
-                <h6><i><b>*Catatan : Tunggu konfirmasi admin paling lambat dua hari kerja untuk verifikasi file.</b></i></h6>
+                <h6><i><b>*Catatan: Tunggu konfirmasi Admin paling lambat dua hari kerja untuk verifikasi file.</b></i></h6>
             </div>
         </div>
     </div>
