470fba356dd8151c0eeb4415cfad72d98e809955
ery <128942912+erutm@users.noreply.github.com>
2024-06-28 21:36:15 +0700

    Update home1.php

--- dashboard/include/home.php
+++ dashboard/include/home.php
@@ -1,36 +1,61 @@
-<h2>Selamat Datang, <?php $role == "Admin" ?  print($nama_admin) : print($nama); ?></h2>
+<!--ery edit heading di admin dan user dan ubah letak kodenya--->
 <?php
 if ($role == 'Admin') {
 ?>
+<h3><b>Selamat Datang, <?php echo $role == "Admin" ?  strtoupper($nama_admin) : ($nama); ?></b></h3>
     <!-- <div class="container"> -->
-    <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-sm-6">
+    <!--- ery edit bagian ini--->
+    <div class="row text-center">
+        <div class="col-lg-6">
             <div class="card card-stats">
                 <div class="card-header" data-background-color="gb">
                     <i class="material-icons">info_outline</i>
                 </div>
                 <div class="card-content">
-                    <p class="category"><b>Pendaftaran Yang Belum DiKonfirmasi</b></p>
+                    <p class="category"><b>Pendaftaran Belum Dikonfirmasi</b></p>
                     <h3 class="title"><?php
-                                        $query      = "SELECT * FROM pendaftaran JOIN detail_pendaftaran ON detail_pendaftaran.id_user = pendaftaran.Id WHERE detail_pendaftaran.status_pendaftaran = 0 AND pendaftaran.upload_surat IS NOT NULL";
-                                        $exec       = mysqli_query($conn, $query);
-                                        $rows = mysqli_num_rows($exec);
-                                        echo $rows;
-                                        ?></h3>
-                </div>
-                <div class="card-footer">
-                    <div class="stats">
-                        <i class="material-icons">date_range</i> <a href="index.php?page=7">More Info</a>
+                                        $query1      = $conn->query("SELECT * FROM pendaftaran JOIN detail_pendaftaran ON detail_pendaftaran.id_user = pendaftaran.Id WHERE status_pendaftaran = 0");
+                                        $blm_konfirm = mysqli_num_rows($query1);
+                                        echo number_format($blm_konfirm);
+                                        ?>
+                    </h3>
+                    <div class="card-footer">
+                    <div class="stats" style="text-align: right;">
+                        <i class="fa fa-info-circle"></i> <a href="index.php?page=7">More Info</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="row text-center">
+        <div class="col-lg-6">
+            <div class="card card-stats">
+                <div class="card-header" data-background-color="gb">
+                    <i class="fa fa-group"></i>
+                </div>
+                <div class="card-content">
+                    <p class="category"><b>Pendaftar Masuk</b></p>
+                    <h3 class="title"><?php
+                                        $query2      = $conn->query("SELECT * FROM pendaftaran JOIN detail_pendaftaran ON detail_pendaftaran.id_user = pendaftaran.Id");
+                                        $total_pendaftar = mysqli_num_rows($query2);
+                                        echo number_format($total_pendaftar);
+                                        ?>
+                    </h3>
+                    <div class="card-footer">
+                    <div class="stats" style="text-align: right;">
+                        <i class="fa fa-info-circle"></i> <a href="index.php?page=7">More Info</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!---sampe sini (nambah yg pendaftar masuk sama edit tata letak kategorinya--->
     <!-- </div> -->
 <?php
 } else {
 ?>
+<!---ery nambah heading buat user sendiri/terpisah ma admin--->
+<h3><b>Selamat Datang, <?php echo $role == "User" ?  strtoupper($nama) : ($nama); ?></b></h3>
     <div class="row text-center">
         <div class="col-md-3"></div>
         <div class="col-md-6">
@@ -62,6 +87,68 @@ if ($role == 'Admin') {
             </div>
         </div>
     </div>
+    <!---ery nambah card baru--->
+    <div class="row text-center">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <div class="card card-stats">
+                <div class="card-header" data-background-color="green">
+                    <h4>Jadwal Magang</h4>
+                </div>
+                <div class="card-content">
+                    <?php
+                    $data_sql = $conn->query("SELECT status_pendaftaran FROM detail_pendaftaran WHERE id_user = $id");
+                    $status_sql = $data_sql->fetch_assoc();
+                    $jadwal = $conn->query("SELECT senin, selasa, rabu, kamis, jumat FROM jadwal WHERE Id = $id_dinas");
+                    $jadwal_sql = $jadwal->fetch_assoc();
+                    if ($status_sql['status_pendaftaran'] == 0) {
+                        echo '<h5><b>Menunggu</b></h5>';
+                    } elseif ($status_sql['status_pendaftaran'] == 1) {
+                        echo '<h5><b>Senin:</b> ' . $jadwal_sql['senin'] . '</h5>';
+                        echo '<h5><b>Selasa:</b> ' . $jadwal_sql['selasa'] . '</h5>';
+                        echo '<h5><b>Rabu:</b> ' . $jadwal_sql['rabu'] . '</h5>';
+                        echo '<h5><b>Kamis:</b> ' . $jadwal_sql['kamis'] . '</h5>';
+                        echo '<h5><b>Jumat:</b> ' . $jadwal_sql['jumat'] . '</h5>';
+                    } else {
+                        echo ' ';
+                    }
+                    ?>
+                </div>
+                <div class="card-footer">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row text-center">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <div class="card card-stats">
+                <div class="card-header" data-background-color="green">
+                    <h4>Tanggal Masuk - Tanggal Keluar</h4>
+                </div>
+                <div class="card-content">
+                    <?php
+                    $sql_status = $conn->query("SELECT status_pendaftaran FROM detail_pendaftaran WHERE id_user = $id");
+                    $status = $sql_status->fetch_assoc();
+                    $sql_data = $conn->query("SELECT tgl_masuk, tgl_keluar FROM pendaftaran WHERE Id = $id_user");
+                    $pendaftaran_data = $sql_data->fetch_assoc(); 
+                        if ($status['status_pendaftaran'] == 0) {
+                            echo '<h5><b>Menunggu</b></h5>';
+                        } elseif ($status['status_pendaftaran'] == 1) {
+                            echo '<h5><b>Tanggal Masuk:</b> ' . $pendaftaran_data['tgl_masuk'] . '</h5>';
+                            echo '<h5><b>Tanggal Keluar:</b> ' . $pendaftaran_data['tgl_keluar'] . '</h5>';
+                        } else {
+                            echo ' ';
+                        }
+                    }
+                    ?>
+                </div>
+                <div class="card-footer">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--- sampe sini-->
 <?php
 }
 ?>
